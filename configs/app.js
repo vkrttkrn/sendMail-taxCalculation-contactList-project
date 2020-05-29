73357172c8a78e010de6e5e24902fe7a94e0f843
Krittikarn Verakalas <35824935+KITkrttkrn@users.noreply.github.com>
2020-05-29 13:19:39 +0700

    Delete app.js

--- configs/app.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  NODE_PORT: "3000",
-  HOST: "localhost",
-  SECRET: "27@Krittikarn08@Verakalas@2540",
-  MAIL_API_KEY:
-    "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",
-};
